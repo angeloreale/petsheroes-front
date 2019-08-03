5d5200064cdbc362f99cf4eddfcdec74aa1e15e1
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2019-08-03 09:03:37 -0300

    Added consumption of City/state API on Home Compenent using vanilla js.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,12 +4,55 @@ import { FormloginComponent } from './../usuarios/formlogin/formlogin.component'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    HomeSingleton.init();
   }
 
+}
+
+const HomeSingleton = {
+  init: function() {
+    this.addListeners();
+    this.loadStates();
+  },
+  addListeners: function() {
+    document.querySelector(".home__c__w__form__estado__select").addEventListener("change", e => {
+      this.loadCities(e.currentTarget.value);
+    }) 
+  },
+  loadStates: function() {
+    let states_el = document.querySelector(".home__c__w__form__estado__select");
+    states_el.innerHTML = `<option value="" disabled selected>Estado</option>`;
+    fetch(`${window.location.protocol}//${window.location.hostname}:3000/api/consulta/cidades`)
+      .then(res => {
+        return res.json()
+      })
+      .then(states => {
+        states.forEach(st => {
+          states_el.innerHTML += `<option value=${st.sigla}>${st.nome}</option>`
+        })
+      });
+  },
+  loadCities: function(state) {
+    let cities_el = document.querySelector(".home__c__w__form__cidade__select")
+    let cities_firstoption = document.querySelector(".home__c__w__form__cidade__select option");
+    cities_firstoption.innerHTML = "Carregando...";
+    fetch(`${window.location.protocol}//${window.location.hostname}:3000/api/consulta/cidades/${state}`)
+    .then(res => {
+      return res.json()
+    })
+    .then(cities => {
+      cities.forEach(ct => {
+        cities_el.innerHTML += `<option value=${ct}>${ct}</option>`
+      });
+      cities_firstoption = document.querySelector(".home__c__w__form__cidade__select option");
+      cities_firstoption.innerHTML = "Cidade";
+      cities_el.disabled = false;
+    });
+  }
 }
